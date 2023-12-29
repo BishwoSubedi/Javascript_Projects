5bbe68b31ac3e8c16e33a975d276fe3cd5607e6c
Bishwo Don <bishwosubedu@gmail.com>
2023-12-29 14:12:32 +0545

    updated

--- HackerRank/QuestionNumber_1.js
+++ HackerRank/QuestionNumber_1.js
@@ -1,62 +1,136 @@
-//Parent Activity class
-function Activity(amount) {
+// //Parent Activity class
+// function Activity(amount) {
+//     this.amount = amount;
+//   }
+  
+//   Activity.prototype.setAmount = function(value) {
+//     if (value <= 0) {
+//       return false;
+//     }
+//     this.amount = value;
+//     return true;
+//   };
+  
+//   Activity.prototype.getAmount = function() {
+//     return this.amount;
+//   };
+  
+//   // Payment class inheriting from Activity
+//   function Payment(amount, receiver) {
+//     Activity.call(this, amount);
+//     this.receiver = receiver;
+//   }
+  
+//   Payment.prototype = Object.create(Activity.prototype);
+  
+//   Payment.prototype.setReceiver = function(value) {
+//     this.receiver = value;
+//   };
+  
+//   Payment.prototype.getReceiver = function() {
+//     return this.receiver;
+//   };
+  
+//   // Refund class inheriting from Activity
+//   function Refund(amount, sender) {
+//     Activity.call(this, amount);
+//     this.sender = sender;
+//   }
+  
+//   Refund.prototype = Object.create(Activity.prototype);
+  
+//   Refund.prototype.setSender = function(value) {
+//     this.sender = value;
+//   };
+  
+//   Refund.prototype.getSender = function() {
+//     return this.sender;
+//   };
+  
+//   const activity = new Activity(100);
+//   console.log(activity.setAmount(150));  
+//   console.log(activity.getAmount());      
+  
+//   const payment = new Payment(50, "John");
+//   payment.setReceiver("Alice");
+//   console.log(payment.getReceiver());  
+//   console.log(payment.getAmount()); 
+  
+//   const refund = new Refund(75, "Bob");
+//   refund.setSender("Charlie");
+//   console.log(refund.getSender());        
+//   console.log(refund.getAmount());     
+
+
+
+
+
+// Alternative Method
+// Parent Activity class
+class Activity {
+  constructor(amount) {
     this.amount = amount;
   }
-  
-  Activity.prototype.setAmount = function(value) {
+
+  setAmount(value) {
     if (value <= 0) {
       return false;
     }
     this.amount = value;
     return true;
-  };
-  
-  Activity.prototype.getAmount = function() {
+  }
+
+  getAmount() {
     return this.amount;
-  };
-  
-  // Payment class inheriting from Activity
-  function Payment(amount, receiver) {
-    Activity.call(this, amount);
+  }
+}
+
+// Payment class inheriting from Activity
+class Payment extends Activity {
+  constructor(amount, receiver) {
+    super(amount); //to access parent constructor
     this.receiver = receiver;
   }
-  
-  Payment.prototype = Object.create(Activity.prototype);
-  
-  Payment.prototype.setReceiver = function(value) {
+
+  setReceiver(value) {
     this.receiver = value;
-  };
-  
-  Payment.prototype.getReceiver = function() {
+  }
+
+  getReceiver() {
     return this.receiver;
-  };
-  
-  // Refund class inheriting from Activity
-  function Refund(amount, sender) {
-    Activity.call(this, amount);
+  }
+}
+
+// Refund class inheriting from Activity
+class Refund extends Activity {
+  constructor(amount, sender) {
+    super(amount);
     this.sender = sender;
   }
-  
-  Refund.prototype = Object.create(Activity.prototype);
-  
-  Refund.prototype.setSender = function(value) {
+
+  setSender(value) {
     this.sender = value;
-  };
-  
-  Refund.prototype.getSender = function() {
+  }
+
+  getSender() {
     return this.sender;
-  };
-  
-  const activity = new Activity(100);
-  console.log(activity.setAmount(150));  
-  console.log(activity.getAmount());      
-  
-  const payment = new Payment(50, "John");
-  payment.setReceiver("Alice");
-  console.log(payment.getReceiver());  
-  console.log(payment.getAmount()); 
-  
-  const refund = new Refund(75, "Bob");
-  refund.setSender("Charlie");
-  console.log(refund.getSender());        
-  console.log(refund.getAmount());     
+  }
+}
+
+
+
+// Example 
+const activity = new Activity(100);
+console.log(activity.setAmount(150));
+console.log(activity.getAmount()); 
+
+const payment = new Payment(50, "Bishwo");
+payment.setReceiver("Ramey");
+console.log(payment.getReceiver());  
+console.log(payment.getAmount());  
+
+const refund = new Refund(75, "Bob");
+refund.setSender("Charlie");
+console.log(refund.getSender());        
+console.log(refund.getAmount());   
+
--- HackerRank/QuestionNumber_2.js
+++ HackerRank/QuestionNumber_2.js
@@ -1,50 +1,98 @@
- // Parent class: Employee
-function Employee(title) {
+//  // Parent class: Employee
+// function Employee(title) {
+//     this.title = title;
+//   }
+  
+//   Employee.prototype.setTitle = function(newTitle) {
+//     this.title = newTitle;
+//   };
+  
+//   Employee.prototype.getTitle = function() {
+//     return this.title;
+//   };
+  
+//   // Child class: Engineer
+//   function Engineer(title, isManager) {
+//     // Call the parent class constructor
+//     Employee.call(this, title);
+  
+//     // Assign additional properties specific to Engineer
+//     this.isManager = isManager;
+//   }
+  
+//   // Inherit from Employee
+//   Engineer.prototype = Object.create(Employee.prototype);
+//   Engineer.prototype.constructor = Engineer;
+  
+//   // Additional functions for Engineer
+//   Engineer.prototype.setManager = function(newIsManager) {
+//     this.isManager = newIsManager;
+//   };
+  
+//   Engineer.prototype.getIsManager = function() {
+//     return this.isManager;
+//   };
+  
+//   // Create an Employee
+//   const employee = new Employee("Software Developer");
+//   console.log(employee.getTitle());  
+  
+//   // Create an Engineer
+//   const engineer = new Engineer("Lead Engineer", true);
+//   console.log(engineer.getTitle());       
+//   console.log(engineer.getIsManager());    
+  
+//   // Update properties
+//   engineer.setTitle("Senior Engineer");
+//   engineer.setManager(false);
+//   console.log(engineer.getTitle());       
+//   console.log(engineer.getIsManager());    
+  
+
+
+  // Alternative method
+  // Parent class: Employee
+class Employee {
+  constructor(title) {
     this.title = title;
   }
-  
-  Employee.prototype.setTitle = function(newTitle) {
+
+  setTitle(newTitle) {
     this.title = newTitle;
-  };
-  
-  Employee.prototype.getTitle = function() {
+  }
+
+  getTitle() {
     return this.title;
-  };
-  
-  // Child class: Engineer
-  function Engineer(title, isManager) {
-    // Call the parent class constructor
-    Employee.call(this, title);
-  
-    // Assign additional properties specific to Engineer
+  }
+}
+
+// Child class: Engineer
+class Engineer extends Employee {
+  constructor(title, isManager) {
+    super(title); // Call the parent class constructor
+
+    // Additional property specific to Engineer
     this.isManager = isManager;
   }
-  
-  // Inherit from Employee
-  Engineer.prototype = Object.create(Employee.prototype);
-  Engineer.prototype.constructor = Engineer;
-  
-  // Additional functions for Engineer
-  Engineer.prototype.setManager = function(newIsManager) {
+
+  setManager(newIsManager) {
     this.isManager = newIsManager;
-  };
-  
-  Engineer.prototype.getIsManager = function() {
+  }
+
+  getIsManager() {
     return this.isManager;
-  };
-  
-  // Create an Employee
-  const employee = new Employee("Software Developer");
-  console.log(employee.getTitle());  
-  
-  // Create an Engineer
-  const engineer = new Engineer("Lead Engineer", true);
-  console.log(engineer.getTitle());       
-  console.log(engineer.getIsManager());    
-  
-  // Update properties
-  engineer.setTitle("Senior Engineer");
-  engineer.setManager(false);
-  console.log(engineer.getTitle());       
-  console.log(engineer.getIsManager());    
-  
+  }
+}
+
+// Example usage
+const employee = new Employee("Software Developer");
+console.log(employee.getTitle());  
+
+const engineer = new Engineer("Lead Engineer", true);
+console.log(engineer.getTitle());  
+console.log(engineer.getIsManager());
+
+engineer.setTitle("Senior Engineer");
+engineer.setManager(false);
+console.log(engineer.getTitle());    
+console.log(engineer.getIsManager()); 
